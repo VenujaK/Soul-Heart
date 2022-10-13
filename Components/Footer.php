eddb457af17c066d244edf9114a37015a5105600
Venuja <93823328+VenujaK@users.noreply.github.com>
2022-10-13 13:57:55 +0530

    Update Footer.php

--- Components/Footer.php
+++ Components/Footer.php
@@ -67,10 +67,10 @@
     <footer>
         <div class="social"><a href="https://www.instagram.com"><i class="icon ion-social-instagram"></i></a><a href="https://www.snapchat.com/"><i class="icon ion-social-snapchat"></i></a><a href="https://twitter.com"><i class="icon ion-social-twitter"></i></a><a href="https://www.facebook.com"><i class="icon ion-social-facebook"></i></a></div>
         <ul class="list-inline">
-            <li class="list-inline-item"><a href="../src/index.php">Home</a></li>
-            <li class="list-inline-item"><a href="../src/Blog.php">Blog</a></li>
-            <li class="list-inline-item"><a href="../src/About_Us.php">About</a></li>
-            <li class="list-inline-item"><a href="../src/Log-Sign.html">Accounts</a></li>
+            <li class="list-inline-item"><a href="index.php">Home</a></li>
+            <li class="list-inline-item"><a href="Blog.php">Blog</a></li>
+            <li class="list-inline-item"><a href="About_Us.php">About</a></li>
+            <li class="list-inline-item"><a href="Log-Sign.html">Accounts</a></li>
         </ul>
         <p class="copyright">Soul heart &copy; 2022</p>
     </footer>
